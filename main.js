fd840acce788fda2929c3f2302f5e9d4c78528e7
Alex Vargas <alexv2489@gmail.com>
2019-01-31 13:00:09 -0500

    add comments

--- main.js
+++ main.js
@@ -13,39 +13,54 @@ let cardsArray = [
   { 'name': 'Wordpress', 'img': 'https://github.com/robgmerrill/img/blob/master/wordpress-logo.png?raw=true', },
 ];
 
+// Duplicate the array to create matches
 let gameGrid = cardsArray.concat(cardsArray);
+
+// Randomize the cards
 gameGrid.sort(function() {
   return 0.5 - Math.random();
 })
 
 let game = document.querySelector('#game-board');
+
+// Create a section element - assign it the class 'grid' and append it to the game board
 let grid = document.createElement('section');
 grid.setAttribute('class', 'grid');
 game.appendChild(grid);
 
+// Loop the cardsArray - create a new div and assign it the class 'card' - assign the index of each name in the array to a dataset of name
 for (i = 0; i < gameGrid.length; i++) {
   let card = document.createElement('div');
   card.classList.add('card');
   card.dataset.name = gameGrid[i].name;
 
+//create a new div and assign it to the variable front and give it a class of 'front'
   let front = document.createElement('div');
   front.classList.add('front');
 
+// create a new div and assign it to the variable back and give it a class of 'back'
   let back = document.createElement('div');
   back.classList.add('back');
+  // add a background image and assign it to the url of each images index
   back.style.backgroundImage = `url(${gameGrid[i].img})`;
 
+// append the card to the grid
   grid.appendChild(card);
+// append the front element to the card
   card.appendChild(front);
+//append the back element to the card
   card.appendChild(back);
 }
 
+// create firstGuess and secondGuess variable and assign it the intial value of a empty string
 let firstGuess = '';
 let secondGuess = '';
+// create a count variable and set it to 0 (check the eventListener)
 let count = 0;
+// create a previousTarget and set it to null
 let previousTarget = null;
-let delay = 1200;
 
+// create a match function which selects all ids with the id of 'selected' that loops through the selected ids and checks for a match
 let match = function() {
   let selected = document.querySelectorAll('.selected');
   for (i = 0; i < selected.length; i++) {
@@ -53,6 +68,7 @@ let match = function() {
   }
 }
 
+// create a reset function after two cards are clicked
 let resetGuesses = function() {
   firstGuess = '';
   secondGuess = '';
@@ -66,6 +82,13 @@ let resetGuesses = function() {
   }
 }
 
+/*create an on click event listener with a couple of conditionals
+  - if the count of the clicked cards is less than 2 increment the count until the conditional is false
+  - if the count is one, the card is assigned to the first guess
+  - else the second card clicked is assigned to the second guess
+  - if the first guess and second match are the same check for a match and then reset guesses
+  - otherwise reset guesses
+*/
 grid.addEventListener('click', function (event) {
   let clicked = event.target;
   if (clicked.nodeName === 'SECTION' || clicked === previousTarget || clicked.parentNode.classList.contains('match') || clicked.parentNode.classList.contains('selected')) {
@@ -82,10 +105,10 @@ grid.addEventListener('click', function (event) {
     }
     if (firstGuess !== '' && secondGuess !== '') {
       if (firstGuess === secondGuess) {
-        setTimeout(match, delay)
-        setTimeout(resetGuesses, delay);
+        setTimeout(match, 1200)
+        setTimeout(resetGuesses, 1200);
       } else {
-        setTimeout(resetGuesses, delay);
+        setTimeout(resetGuesses, 1200);
       }
     }
     previousTarget = clicked
